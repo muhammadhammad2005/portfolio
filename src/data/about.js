e94fe91f7aaf0abe332c1d387359a2f7a5beaee1
muhammadhammad2005 <youyt.manager@gmail.com>
2025-08-13 00:27:11 +0500

    Updates

--- src/data/about.js
+++
@@ -1,9 +0,0 @@
-const AboutData = {
-  image: require("../assets/Images/about.png"),
-  description: [
-    "A dedicated and self-driven DevOps enthusiast with hands-on experience in cloud infrastructure, automation, and system administration.",
-    "Skilled in using Docker and Kubernetes for containerization and orchestration. I specialize in building CI/CD pipelines with GitHub Actions, and automating infrastructure using Terraform and Ansible.",
-    "Passionate about Linux, scripting with Bash, Python & YAML, and simplifying deployment workflows. I'm always eager to take on new challenges and explore innovative cloud solutions.",
-  ],
-};
-export default AboutData;
